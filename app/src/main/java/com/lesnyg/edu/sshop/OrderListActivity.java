1ea0d618fbc3441decc3006dddcd0ad10782fa78
Sunnyiee <rlatjsdl002@gmail.com>
2018-11-06 16:20:09 +0900

    commit s-1

--- app/src/main/java/com/lesnyg/edu/sshop/OrderListActivity.java
+++ app/src/main/java/com/lesnyg/edu/sshop/OrderListActivity.java
@@ -1,13 +1,30 @@
 package com.lesnyg.edu.sshop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class OrderListActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+
+public class OrderListActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_list);
+        setContentView(R.layout.activity_edit_menu);
+
+        Button btorderlist01 = findViewById(R.id.btorderlist01);
+        btorderlist01.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()){
+            case R.id.btorderlist01:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent); break;
+        }}
 }
